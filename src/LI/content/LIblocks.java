78e33dc6390d21a73199ed7d16cb689a105f258c
lu69159 <2909165527@qq.com>
2026-05-04 00:15:14 +0800

    更新方块中

--- src/LI/content/LIblocks.java
+++ src/LI/content/LIblocks.java
@@ -78,10 +78,13 @@ public class LIblocks {
     ;
 
     public static void load(){
+        //region 单位
         BCJDWZGGC = new Reconstructor("倍乘级单位直构工厂"){{
             requirements(Category.units, with(Items.lead, 600, Items.titanium, 300, Items.thorium, 200, Items.silicon, 400, LIitems.ZYZ, 75));
             health = 1200;
             size = 5;
+            consumePower(6f);
+            consumeItems(with(Items.silicon, 130, LIitems.ZYZ, 30));
             upgrades.addAll(
                 new UnitType[]{UnitTypes.dagger, UnitTypes.fortress},
                 new UnitType[]{UnitTypes.nova, UnitTypes.quasar},
@@ -94,5 +97,75 @@ public class LIblocks {
             );
             constructTime = 1800f;
         }};
-    };
+        DMJDWZGGC = new Reconstructor("多幂级单位直构工厂"){{
+            requirements(Category.units, with(Items.lead, 1800, Items.titanium, 1800, Items.thorium, 350, Items.silicon, 800, Items.plastanium, 450, Items.phaseFabric, 300, LIitems.ZYZ, 375));
+            health = 3035;
+            size = 7;
+            liquidCapacity = 60f;
+            consumePower(13f);
+            consumeItems(with(Items.silicon, 850, LIitems.ZYZ, 350));
+            consumeLiquid(Liquids.cryofluid, 1f);
+            upgrades.addAll(
+                new UnitType[]{UnitTypes.dagger, UnitTypes.scepter},
+                new UnitType[]{UnitTypes.nova, UnitTypes.vela},
+                new UnitType[]{UnitTypes.crawler, UnitTypes.arkyid},
+                new UnitType[]{UnitTypes.flare, UnitTypes.antumbra},
+                new UnitType[]{UnitTypes.mono, UnitTypes.quad},
+                new UnitType[]{UnitTypes.risso, UnitTypes.sei},
+                new UnitType[]{UnitTypes.retusa, UnitTypes.aegires},
+                new UnitType[]{LIunits.SM, LIunits.DY}
+            );
+            constructTime = 5400f;
+        }};
+        WLJDWZGGC = new Reconstructor("多幂级单位直构工厂"){{
+            requirements(Category.units, with(Items.lead, 4000, Items.thorium, 500, Items.silicon, 3000, Items.plastanium, 600, Items.phaseFabric, 325, Items.surgeAlloy, 800, LIitems.ZYZ, 400));
+            health = 5830;
+            size = 9;
+            liquidCapacity = 180f;
+            consumePower(25f);
+            consumeItems(with(Items.silicon, 1000, LIitems.ZYZ, 630));
+            consumeLiquid(Liquids.cryofluid, 3f);
+            upgrades.addAll(
+                new UnitType[]{UnitTypes.dagger, UnitTypes.reign},
+                new UnitType[]{UnitTypes.nova, UnitTypes.corvus},
+                new UnitType[]{UnitTypes.crawler, UnitTypes.toxopid},
+                new UnitType[]{UnitTypes.flare, UnitTypes.eclipse},
+                new UnitType[]{UnitTypes.mono, UnitTypes.oct},
+                new UnitType[]{UnitTypes.risso, UnitTypes.omura},
+                new UnitType[]{UnitTypes.retusa, UnitTypes.navanax},
+                new UnitType[]{LIunits.SM, LIunits.JX}
+            );
+            constructTime = 14400f;
+        }};
+        FZGC = new UnitFactory("辅助工厂"){{
+            requirements(Category.units, with(Items.copper, 60, Items.lead, 80, Items.plastanium, 15));
+            health = 540;
+            size = 3;
+            consumePower(3f);
+            plans = Seq.with(
+                new UnitPlan(LIunits.ZLQ, 60f * 30, with(Items.silicon, 15, LIitems.ZYZ, 1)),
+                new UnitPlan(LIunits.XFQ, 60f * 30, with(Items.silicon, 10, Items.phaseFabric, 2)),
+                new UnitPlan(LIunits.JZQ, 60f * 30, with(Items.silicon, 40)),
+                new UnitPlan(LIunits.ZTQ, 60f * 30, with(Items.silicon, 5, LIitems.QSZ, 3, LIitems.ZYZ, 1))
+            );
+        }};
+        GZQ = new Constructor("构筑器"){{
+            requirements(Category.units, with(Items.thorium, 80, Items.silicon, 150, Items.plastanium, 15));
+            health = 800;
+            size = 5;
+            buildSpeed = 2f;
+            hasPower = true;
+            minBlockSize = 2;
+            maxBlockSize = 5;
+            consumePower(4f);
+        }};
+        DXZHCSD = new PayloadConveyor("大型载荷传送带"){{
+            requirements(Category.units, with(Items.copper, 30, Items.graphite, 30));
+            health = 800;
+            size = 5;
+            canOverdrive = false;
+        }};
+
+        //region 电力
+    }
 }
